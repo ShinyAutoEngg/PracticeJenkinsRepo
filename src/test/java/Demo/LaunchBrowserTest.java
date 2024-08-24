9c8abebb028dc83f48c5544cd31f9f29e83bcae2
ShinyAutoEngg <shinyingrid@gmail.com>
2024-08-24 16:03:59 +0530

    push 1

---
+++ src/test/java/Demo/LaunchBrowserTest.java
@@ -0,0 +1,19 @@
+package Demo;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LaunchBrowserTest
+{
+	@Test
+	public void demoTest()
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.get("https://www.amazon.in/");
+	}
+}
